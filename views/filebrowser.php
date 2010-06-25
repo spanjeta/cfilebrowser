249ef64a2f267892f978c43c14df24a0a8a22dbd
kbradwick <kbradwick@2e63cd0d-9ceb-3947-b557-e4ef2dd7fe21>
2010-06-25 10:22:03 +0000

    version 1 with documentation complete

--- views/filebrowser.php
+++ views/filebrowser.php
@@ -1,33 +1,27 @@
-<?php
-
-/**
- * This file will render the file browser elements
- * on the page
- */
-
-
-?>
-
 <div id="<?php echo $this->containerID; ?>"></div>
 
 <script type="text/javascript">
 //<![CDATA[
-jQuery(document).ready(function($) {
-	$('#<?php echo $this->containerID; ?>').fileTree({
+jQuery(document).ready(function() {
+	jQuery('#<?php echo $this->containerID; ?>').fileTree({
 		root:'<?php echo $this->root; ?>',
 		script:'<?php echo $script; ?>',
 		folderEvent:'<?php echo $this->folderEvent; ?>',
 		expandSpeed:<?php echo $this->expandSpeed; ?>,
 		collapseSpeed:<?php echo $this->collapseSpeed; ?>,
 		multiFolder:<?php echo $this->multiFolder ? 'true' : 'false'; ?>,
-		loadMessage:'<?php echo $this->loadMessage; ?>',
+		<?php if(!empty($this->expandEasing)) : ?>
 		expandEasing:'<?php echo $this->expandEasing; ?>',
-		collapseEasing:'<?php echo $this->collapseEasing; ?>'
+		<?php endif; ?>
+		<?php if(!empty($this->collapseEasing)) : ?>
+		collapseEasing:'<?php echo $this->collapseEasing; ?>',
+		<?php endif; ?>
+		loadMessage:'<?php echo $this->loadMessage; ?>'
 	}, function(f){
 		<?php if(empty($this->callbackFunction)) : ?>
 		alert(f);
 		<?php else : ?>
-		<?php echo $this->callbackFunction; ?>(f);
+		<?php echo substr($this->callbackFunction,-1) == ';' ? $this->callbackFunction : $this->callbackFunction.';'; ?>
 		<?php endif; ?>
 	});
 });
